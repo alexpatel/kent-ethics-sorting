f4d8ce61c168024d087d627958223b4441f9d185
alexhpatel <alexanderpatel@college.harvard.edu>
2014-01-13 21:42:30 -0700

    models, config vars

---
+++ sort.py
@@ -0,0 +1,24 @@
+'''
+sort.py 
+Alexander Patel - alexanderpatel@college.harvard.edu
+January 13, 2014
+'''
+
+import models
+
+'''
+Configuration
+'''
+# optimal seminar size 
+opt_size = 15
+
+# maximum seminar size
+max_size = 20
+
+# percentage weight for age
+# e.g. age_distro = 10 --> seniors are 10% more likely to receive 
+# first choice than juniors, juniors 10% more likely than sophomores, etc.
+age_distro = 0
+
+
+
